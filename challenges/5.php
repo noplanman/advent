9d012980146a5242a644c4e24b93764a559a21e3
Armando Lüscher <armando@noplanman.ch>
2015-12-05 13:07:42 +0100

    Add new nice string checks for part 2.

--- challenges/5.php
+++ challenges/5.php
@@ -19,14 +19,14 @@ class Challenge5 extends Challenge {
 	protected static $_day = 5;
 
 	/**
-	 * The number of nice strings.
+	 * The number of nice strings for each part.
 	 *
-	 * @var integer
+	 * @var array
 	 */
-	private $_nice_strings = 0;
+	private $_nice_strings = [ 1 => 0, 2 => 0 ];
 
 	/**
-	 * Check if the string has any illegal letter combinations.
+	 * Check if the string has any illegal letter combinations. (For part 1)
 	 *
 	 * @param string  $string The string to check.
 	 * @return boolean If this check passes.
@@ -42,7 +42,7 @@ class Challenge5 extends Challenge {
 	}
 
 	/**
-	 * Check if the string has at least 3 vowels.
+	 * Check if the string has at least 3 vowels. (For part 1)
 	 *
 	 * @param string  $string The string to check.
 	 * @return boolean If this check passes.
@@ -57,7 +57,7 @@ class Challenge5 extends Challenge {
 	}
 
 	/**
-	 * Check if the string has a double letter.
+	 * Check if the string has a double letter. (For part 1)
 	 *
 	 * @param string  $string The string to check.
 	 * @return boolean If this check passes.
@@ -72,21 +72,68 @@ class Challenge5 extends Challenge {
 	}
 
 	/**
+	 * Check if the string has at least two letter pairs. (For part 1)
+	 *
+	 * @param string  $string The string to check.
+	 * @return boolean If this check passes.
+	 */
+	private function _has_two_pairs( $string ) {
+		for ( $pos = 0; $pos < strlen( $string ) -1; $pos++ ) {
+			// Get each pair and explode the string with it.
+			$pair = substr( $string, $pos, 2 );
+			// If we have more than 2 entries, we have two valid pairs.
+			if ( count( explode( $pair, $string ) ) > 2 ) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Check if the string has a letter sandwich. (For part 2)
+	 *
+	 * @param string  $string The string to check.
+	 * @return boolean If this check passes.
+	 */
+	private function _has_letter_sandwich( $string ) {
+		for ( $pos = 0; $pos < strlen( $string ) - 2; $pos++ ) {
+			if ( $string[ $pos ] === $string[ $pos + 2 ] ) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
 	 * The main method where the challenge gets solved.
 	 */
 	public function solve() {
 		$all_strings = explode( "\n", $this->_input );
 
-		$checks = [ '_has_no_illegals', '_has_three_vowels', '_has_double_letter' ];
+		$checks_part_1 = [ '_has_no_illegals', '_has_three_vowels', '_has_double_letter' ];
+		$checks_part_2 = [ '_has_two_pairs', '_has_letter_sandwich' ];
 
 		foreach ( $all_strings as $string ) {
-			// Only increment nice string counter if all checks pass.
-			foreach ( $checks as $check ) {
+			// Only increment nice string counters if all checks pass.
+			// Part 1.
+			$valid = true;
+			foreach ( $checks_part_1 as $check ) {
 				if ( ! call_user_func( [ $this, $check ], $string ) ) {
-					continue 2;
+					$valid = false;
+					break;
 				}
 			}
-			$this->_nice_strings++;
+			$valid && $this->_nice_strings[1]++;
+
+			// Part 2.
+			$valid = true;
+			foreach ( $checks_part_2 as $check ) {
+				if ( ! call_user_func( [ $this, $check ], $string ) ) {
+					$valid = false;
+					break;
+				}
+			}
+			$valid && $this->_nice_strings[2]++;
 		}
 	}
 
@@ -94,13 +141,13 @@ class Challenge5 extends Challenge {
 	 * Output the solution for part 1.
 	 */
 	public function output_part_1() {
-		echo 'Nice strings: ' . $this->_nice_strings;
+		echo 'Nice strings for part 1: ' . $this->_nice_strings[1];
 	}
 
 	/**
 	 * Output the solution for part 2.
 	 */
 	public function output_part_2() {
-		echo '';
+		echo 'Nice strings for part 2: ' . $this->_nice_strings[2];
 	}
 }
