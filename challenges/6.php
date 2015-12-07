18bce8a77109fd40947f7285b3cae7bc87ed3c71
Armando Lüscher <armando@noplanman.ch>
2015-12-07 19:58:51 +0100

    Add solution for day 6 challenge, part 2. Such a big array is really some piece of work for PHP...

--- challenges/6.php
+++ challenges/6.php
@@ -19,14 +19,21 @@ class Challenge6 extends Challenge {
 	protected static $_day = 6;
 
 	/**
-	 * Array of light data.
+	 * Array of light status data. (For part 1)
 	 *
 	 * @var array
 	 */
 	private $_lights = [];
 
 	/**
-	 * Toggle the lights that fit the passed parameters.
+	 * Array of light brightness data. (For part 2)
+	 *
+	 * @var array
+	 */
+	private $_lights_brightness = [];
+
+	/**
+	 * Toggle the lights that fit the passed parameters. (For part 1)
 	 *
 	 * @param string $toggle The toggle type: on, off, toggle.
 	 * @param string $from   The X,Y coordinate to start from.
@@ -48,7 +55,7 @@ class Challenge6 extends Challenge {
 	}
 
 	/**
-	 * Get the number of lights that are switched on.
+	 * Get the number of lights that are switched on. (For part 1)
 	 *
 	 * @return integer The number of lights that are switched on.
 	 */
@@ -63,12 +70,49 @@ class Challenge6 extends Challenge {
 	}
 
 	/**
+	 * Toggle the brightness of the lights that fit the passed parameters. (For part 2)
+	 *
+	 * @param string $toggle The toggle type: on, off, toggle.
+	 * @param string $from   The X,Y coordinate to start from.
+	 * @param string $to     The X,Y coordinate to end at.
+	 */
+	private function _toggle_lights_brightness( $toggle, $from, $to ) {
+		list( $from_x, $from_y ) = explode( ',', $from );
+		list( $to_x, $to_y ) = explode( ',', $to );
+		for ( $x = $from_x; $x <= $to_x; $x++ ) {
+			for ( $y = $from_y; $y <= $to_y; $y++ ) {
+				$lb = ( isset( $this->_lights_brightness[ $x ][ $y ] ) ) ? $this->_lights_brightness[ $x ][ $y ] : 0;
+				// Depending on the toggle passed, set the brightness accordingly.
+				( 'on' === $toggle ) && $lb++;
+				( 'off' === $toggle ) && $lb--;
+				( 'toggle' === $toggle ) && $lb += 2;
+				$this->_lights_brightness[ $x ][ $y ] = max( 0, $lb );
+			}
+		}
+	}
+
+	/**
+	 * Get the brightness of all the lights combined.
+	 *
+	 * @return integer The brightness of all lights combined.
+	 */
+	private function _get_lights_brightness() {
+		$brightness = 0;
+		foreach ( $this->_lights_brightness as $x ) {
+			foreach ( $x as $y ) {
+				$brightness += $y;
+			}
+		}
+		return $brightness;
+	}
+
+	/**
 	 * The main method where the challenge gets solved.
 	 */
 	public function solve() {
 		// Because this is a monster array operation, we need more resources...
-		ini_set( 'memory_limit', '256M' );
-		ini_set( 'max_execution_time', 60 );
+		ini_set( 'memory_limit', '384M' );
+		ini_set( 'max_execution_time', 120 );
 
 		// Remove "turn " and "through " from the strings to read the important infos better.
 		$instructions = str_replace( [ 'turn ', 'through ' ], '', $this->_input );
@@ -76,6 +120,7 @@ class Challenge6 extends Challenge {
 		foreach ( $instructions as $instruction ) {
 			list( $toggle, $from, $to ) = explode( ' ', $instruction );
 			$this->_toggle_lights( $toggle, $from, $to );
+			$this->_toggle_lights_brightness( $toggle, $from, $to );
 		}
 	}
 
@@ -90,6 +135,6 @@ class Challenge6 extends Challenge {
 	 * Output the solution for part 2.
 	 */
 	public function output_part_2() {
-		echo '';
+		echo 'Light brightness: ' . $this->_get_lights_brightness();
 	}
 }
