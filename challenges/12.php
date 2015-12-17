9b24cd5690db3ee417ec6cafb0e1bc32f921560a
Armando Lüscher <armando@noplanman.ch>
2015-12-17 05:37:52 +0100

    Add solution for day 12 challenge, part 2. Just couldn't get array_walk_recursive to work, so I borrowed the regex from another solution.

--- challenges/12.php
+++ challenges/12.php
@@ -19,32 +19,40 @@ class Challenge12 extends Challenge {
 	protected static $_day = 12;
 
 	/**
-	 * The sum of all numbers found in the input.
+	 * The sums of all numbers that should be counted for both part 1 and 2.
 	 *
-	 * @var integer
+	 * @var array
 	 */
-	private $_sum = 0;
+	private $_sums = [];
 
 	/**
 	 * The main method where the challenge gets solved.
 	 */
 	public function solve() {
 		$json = $this->_input;
-		preg_match_all( '/\-?\d+/', $json, $m );
-		$this->_sum = array_sum( $m[0] );
+
+		// For part 1, count ALL numbers.
+		preg_match_all( '/-?\d+/', $json, $m );
+		$this->_sums[1] = array_sum( $m[0] );
+
+		// For part 2, only the ones that aren't in a "red" object.
+		// Regex from: https://github.com/ChrisPenner/Advent-Of-Code-Polyglot/blob/master/erlang/12/part2.erl
+		$json = preg_replace( '/{(\[(\[(?2)*]|{(?2)*}|[^][}{])*]|{(?2)*}|[^][}{])*red(?1)*}/', '', $json );
+		preg_match_all( '/-?\d+/', $json, $m );
+		$this->_sums[2] = array_sum( $m[0] );
 	}
 
 	/**
 	 * Output the solution for part 1.
 	 */
 	public function output_part_1() {
-		echo 'Sum of all numbers: ' . $this->_sum;
+		echo 'Sum of all numbers: ' . $this->_sums[1];
 	}
 
 	/**
 	 * Output the solution for part 2.
 	 */
 	public function output_part_2() {
-		echo '';
+		echo 'Sum of all non red numbers: ' . $this->_sums[2];
 	}
 }
