286d13c710e3081f7e39bc3703fdb76f56ea6ec5
Armando Lüscher <armando@noplanman.ch>
2015-12-18 14:38:33 +0100

    Add solution for day 16 challenge, part 2.

--- challenges/16.php
+++ challenges/16.php
@@ -37,11 +37,11 @@ class Challenge16 extends Challenge {
 	];
 
 	/**
-	 * The number of the auntie the present is from.
+	 * The numbers of the auntie the present is from, for both part 1 and 2.
 	 *
-	 * @var integer
+	 * @var array
 	 */
-	private $_present_auntie_nr = 0;
+	private $_present_auntie_nrs = [ 1 => 0, 2 => 0 ];
 
 	/**
 	 * The main method where the challenge gets solved.
@@ -53,17 +53,29 @@ class Challenge16 extends Challenge {
 			array_shift( $auntie_compounds );
 			$auntie_nr = array_shift( $auntie_compounds );
 
-			$perfect_fit = true;
+			$finding_auntie_1 = ! (bool) $this->_present_auntie_nrs[1];
+			$finding_auntie_2 = ! (bool) $this->_present_auntie_nrs[2];
+
 			for ( $i = 0; $i < count( $auntie_compounds ); $i += 2 ) {
-				if ( (int) $auntie_compounds[ $i + 1 ] !== $this->_present_data[ $auntie_compounds[ $i ] ] ) {
-					$perfect_fit = false;
-					break;
+				$compound_name = $auntie_compounds[ $i ];
+				$compound_value = (int) $auntie_compounds[ $i + 1 ];
+
+				// Find auntie for part 1, where compounds must match perfectly.
+				( $finding_auntie_1 && $compound_value !== $this->_present_data[ $compound_name ] ) && $finding_auntie_1 = false;
+
+				// Find auntie for part 2, where compounds must match according to the correct MFCSAM instructions.
+				if ( $finding_auntie_2 ) {
+					if ( in_array( $compound_name, [ 'cats', 'trees' ] ) ) {
+						( $compound_value <= $this->_present_data[ $compound_name ] ) && $finding_auntie_2 = false;
+					} elseif ( in_array( $compound_name, [ 'pomeranians', 'goldfish' ] ) ) {
+						( $compound_value >= $this->_present_data[ $compound_name ] ) && $finding_auntie_2 = false;
+					} else {
+						( $compound_value !== $this->_present_data[ $compound_name ] ) && $finding_auntie_2 = false;
+					}
 				}
 			}
-			if ( $perfect_fit ) {
-				$this->_present_auntie_nr = $auntie_nr;
-				break;
-			}
+			( $finding_auntie_1 ) && $this->_present_auntie_nrs[1] = $auntie_nr;
+			( $finding_auntie_2 ) && $this->_present_auntie_nrs[2] = $auntie_nr;
 		}
 	}
 
@@ -71,13 +83,13 @@ class Challenge16 extends Challenge {
 	 * Output the solution for part 1.
 	 */
 	public function output_part_1() {
-		echo 'The present is from auntie Sue number ' . $this->_present_auntie_nr;
+		echo 'The present is from auntie Sue number ' . $this->_present_auntie_nrs[1];
 	}
 
 	/**
 	 * Output the solution for part 2.
 	 */
 	public function output_part_2() {
-		echo '';
+		echo 'With correct usage, the present is from auntie Sue number ' . $this->_present_auntie_nrs[2];
 	}
 }
