913d85df0c53a71c6a9d0af33a2d6e3876f76fc3
Armando Lüscher <armando@noplanman.ch>
2016-12-07 00:39:08 +0100

    Add challenge 2, part 2. Add separate helper to find next step. Clean up code.

--- 2016/src/Challenges/Challenge2.php
+++ 2016/src/Challenges/Challenge2.php
@@ -29,16 +29,41 @@ class Challenge2 extends ChallengeBase
     private $part_2;
 
     /**
-     * The main method where the challenge gets solved.
+     * Calculate the next valid step.
+     * 
+     * @param $pad
+     * @param $x
+     * @param $y
+     * @param $dir
+     * @return array
      */
-    public function solve()
+    private function next_step($pad, $x, $y, $dir)
     {
-        $moves = explode(PHP_EOL, trim($this->input));
+        $x2 = $x;
+        $y2 = $y;
+        
+        ('U' === $dir) && $y2--;
+        ('R' === $dir) && $x2++;
+        ('D' === $dir) && $y2++;
+        ('L' === $dir) && $x2--;
 
+        $x = max(0, min(count($pad) - 1, $x));
+        $y = max(0, min(count($pad) - 1, $y));
+
+        return (empty($pad[$y2][$x2]) || ' ' === $pad[$y2][$x2]) ? [$x, $y] : [$x2, $y2];
+    }
+
+    /**
+     * Solve part 1 and save the answer to $this->part_1.
+     *
+     * @param array $moves
+     */
+    private function solve_1(array $moves)
+    {
         $pad = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9]
+            ['1', '2', '3'],
+            ['4', '5', '6'],
+            ['7', '8', '9']
         ];
 
         $x = 1;
@@ -47,16 +72,48 @@ class Challenge2 extends ChallengeBase
         foreach ($moves as $move) {
             $steps = str_split($move);
             foreach ($steps as $step) {
-                ('U' === $step) && $y--;
-                ('R' === $step) && $x++;
-                ('D' === $step) && $y++;
-                ('L' === $step) && $x--;
-
-                $x = max(0, min(2, $x));
-                $y = max(0, min(2, $y));
+                list($x, $y) = $this->next_step($pad, $x, $y, $step);
             }
             $this->part_1 .= $pad[$y][$x];
         }
+    }
+    
+    /**
+     * Solve part 2 and save the answer to $this->part_2.
+     *
+     * @param array $moves
+     */
+    private function solve_2(array $moves)
+    {
+        $pad = [
+            [' ', ' ', '1', ' ', ' '],
+            [' ', '2', '3', '4', ' '],
+            ['5', '6', '7', '8', '9'],
+            [' ', 'A', 'B', 'C', ' '],
+            [' ', ' ', 'D', ' ', ' ']
+        ];
+
+        $x = 0;
+        $y = 2;
+
+        foreach ($moves as $move) {
+            $steps = str_split($move);
+            foreach ($steps as $step) {
+                list($x, $y) = $this->next_step($pad, $x, $y, $step);
+            }
+            $this->part_2 .= $pad[$y][$x];
+        }
+    }
+
+    /**
+     * The main method where the challenge gets solved.
+     */
+    public function solve()
+    {
+        $moves = explode(PHP_EOL, trim($this->input));
+
+        $this->solve_1($moves);
+        $this->solve_2($moves);
     }
 
     /**
@@ -64,7 +121,7 @@ class Challenge2 extends ChallengeBase
      */
     public function output_part_1()
     {
-        echo 'Bathroom code is: ' . $this->part_1;
+        echo 'Part 1 bathroom code is: ' . $this->part_1;
     }
 
     /**
@@ -72,6 +129,6 @@ class Challenge2 extends ChallengeBase
      */
     public function output_part_2()
     {
-        echo '';
+        echo 'Part 2 bathroom code is: ' . $this->part_2;
     }
 }
