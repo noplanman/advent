7739c05419333dfc3b69d31db850f07d18e12fa2
Armando Lüscher <armando@noplanman.ch>
2016-12-06 23:51:46 +0100

    Add challenge 1, part 2. Clean up code.

--- 2016/src/Challenges/Challenge1.php
+++ 2016/src/Challenges/Challenge1.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types = 1);
 /**
  * Challenge for day 1 of Advent of Code.
- * http://adventofcode.com/day/1
+ * http://adventofcode.com/2016/day/1
  *
  * @package AOC_Solutions
  */
@@ -14,13 +14,19 @@ namespace AOC\Challenges;
 class Challenge1 extends ChallengeBase
 {
     /**
-     * The current challenge day.
-     *
-     * @var integer
+     * @var integer The current challenge day.
      */
     protected static $day = 1;
 
+    /**
+     * @var int Solution for part 1.
+     */
     private $part_1;
+
+    /**
+     * @var int Solution for part 2.
+     */
+    private $part_2;
 
     /**
      * The main method where the challenge gets solved.
@@ -28,16 +34,36 @@ class Challenge1 extends ChallengeBase
     public function solve()
     {
         $directions = explode(',', str_replace(' ', '', $this->input));
-        $moves = array_fill(0, 4, 0);
-        $cur_dir = 0;
+        
+        $x = 0;
+        $y = 0;
+        $cur_direction = 0;
+        $distance = 0;
+
+        $positions = [];
         foreach ($directions as $direction) {
-            $cur_dir += $direction[0] === 'R' ? 1 : -1;
-            ($cur_dir < 0) && $cur_dir += 4;
-            $cur_dir %= 4;
-            $moves[$cur_dir] += (int)substr($direction, 1);
+            $cur_direction += $direction[0] === 'R' ? 1 : -1;
+            ($cur_direction < 0) && $cur_direction += 4;
+            $cur_direction %= 4;
+            
+            $cur_distance = (int)substr($direction, 1);
+            // We need to remember every individual step.
+            while ($cur_distance--) {
+                (0 === $cur_direction) && $y++;
+                (1 === $cur_direction) && $x++;
+                (2 === $cur_direction) && $y--;
+                (3 === $cur_direction) && $x--;
+
+                $position = "$x:$y";
+                $distance = abs($x) + abs($y);
+                if (null === $this->part_2 && in_array($position, $positions, true)) {
+                    $this->part_2 = $distance;
+                }
+                $positions[] = $position;
+            }
         }
 
-        $this->part_1 = abs($moves[0] - $moves[2]) + abs($moves[1] - $moves[3]);
+        $this->part_1 = $distance;
     }
 
     /**
@@ -53,6 +79,6 @@ class Challenge1 extends ChallengeBase
      */
     public function output_part_2()
     {
-        echo '';
+        echo 'First double-visit is ' . $this->part_2 . ' blocks away.';
     }
 }
